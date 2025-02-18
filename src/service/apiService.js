defbc48b8b6895118210d2263b7a75dd21c451af
Seva <admin@mail.ru>
2025-02-18 07:26:22 +0300

    fixing code apiService

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -3,35 +3,56 @@ export default class ApiService {
 
   #baseURL = 'https://api.themoviedb.org/'
 
-  #baseURLImage = 'https://image.tmdb.org/t/p/w500/'
+  // #baseURLImage = 'https://image.tmdb.org/t/p/w500/'
+
+  async getResource(url) {
+    const res = await fetch(url)
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received status ${res.status}`)
+    }
+
+    this.resource = await res.json()
+
+    return this.resource
+  }
 
   async getMovies(page) {
-    const url = new URL('3/trending/movie/day', this.#baseURL)
-    url.searchParams.set('api_key', this.#apiKey)
-    url.searchParams.set('page', page)
+    const url = this.createUrlMovies(page)
+    const result = await this.getResource(url)
 
-    const result = await fetch(`${url}`)
-
-    return result.json()
+    return result
   }
 
   async getSearchMovies(searchWords, page) {
+    const url = this.createUrlSearchMovies(searchWords, page)
+    const result = await this.getResource(url)
+
+    return result
+  }
+
+  createUrlSearchMovies(searchWords, page) {
     const url = new URL('3/search/movie', this.#baseURL)
     url.searchParams.set('api_key', this.#apiKey)
     url.searchParams.set('query', searchWords)
     url.searchParams.set('page', page)
-    // url.searchParams.set('include_adult', false)
-    // url.searchParams.set('language', 'en-US')
-    const result = await fetch(`${url}`)
 
-    return result.json()
+    return url
   }
 
-  async getOneImage(imgUrl) {
-    const url = new URL(imgUrl, this.#baseURLImage)
+  createUrlMovies(page) {
+    const url = new URL('3/trending/movie/day', this.#baseURL)
+    url.searchParams.set('api_key', this.#apiKey)
+    url.searchParams.set('page', page)
 
-    const result = await fetch(`${url}`)
-
-    return result.json()
+    return url
   }
+
+  // async getOneImage(imgUrl) {
+  //   const url = new URL(imgUrl, this.#baseURLImage)
+
+  //   const result = await fetch(`${url}`)
+
+  //   return result.json()
+  // }
 }

e876db0a673377cfb93cf6a22279d2a0a07ee1e8
Seva <admin@mail.ru>
2025-02-28 20:21:44 +0300

    add tab on App.js / create ApiContext. Provider

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,8 +1,30 @@
+import { useMemo } from 'react'
 import './App.module.scss'
-import Page from '../Page/Page'
+import { Tabs } from 'antd'
+
+import SearchPage from '../Page/SearchPage/SearchPage'
+import RatePage from '../Page/RatePage/RatePage'
+import ApiService from '../../service/ApiService'
+import ApiContext from '../../service/Context'
+
+const items = [
+  { key: 1, label: 'Search', children: <SearchPage /> },
+  { key: 2, label: 'Rated', children: <RatePage /> },
+]
 
 function App() {
-  return <Page />
+  const api = useMemo(() => new ApiService(), [])
+
+  return (
+    <ApiContext.Provider value={api}>
+      <Tabs
+        defaultActiveKey="1"
+        destroyInactiveTabPane
+        items={items}
+        centered
+      />
+    </ApiContext.Provider>
+  )
 }
 
 export default App
